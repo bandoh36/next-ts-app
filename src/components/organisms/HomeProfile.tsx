5aece226ea15363ee84b6c01a7836e1eab8bdee4
akisute36 <bandoh4321@gmail.com>
2023-10-29 12:49:03 +0900

    test: NextImageOptimizedTest

--- src/components/organisms/HomeProfile.tsx
+++ src/components/organisms/HomeProfile.tsx
@@ -1,5 +1,17 @@
+import Box from "@mui/system/Box";
+import Image from "next/image";
+
 const HomeProfile = () => {
-  return <>Profile</>;
+  return (
+    <Box>
+      <Image
+        src="/image/profile/tsuri.jpg"
+        alt="fishing"
+        width={100}
+        height={100}
+      />
+    </Box>
+  );
 };
 
 export default HomeProfile;
