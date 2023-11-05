9e5fd0366903bd9a5eee275131fefde650c5d8a9
akisute36 <bandoh4321@gmail.com>
2023-11-06 01:42:35 +0900

    feat: Skillset記載

--- src/components/organisms/HomeSkillset.tsx
+++ src/components/organisms/HomeSkillset.tsx
@@ -1,51 +1,17 @@
-import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
 
 import SkillsetList from "@/components/molecules/SkillsetList";
 
 const HomeSkillset = () => {
   return (
-    <Container maxWidth="md" sx={{ mt: 3, mb: 3 }}>
-      <Grid container spacing={5} sx={{ minWidth: "650px" }}>
-        <Grid item xs={12}>
-          <Paper
-            sx={{
-              p: 4,
-              display: "flex",
-              flexDirection: "column",
-              boxShadow: "6",
-            }}
-          >
-            <SkillsetList id="front" />
-          </Paper>
-        </Grid>
-        <Grid item xs={12}>
-          <Paper
-            sx={{
-              p: 4,
-              display: "flex",
-              flexDirection: "column",
-              boxShadow: "6",
-            }}
-          >
-            <SkillsetList id="back" />
-          </Paper>
-        </Grid>
-        <Grid item xs={12}>
-          <Paper
-            sx={{
-              p: 4,
-              display: "flex",
-              flexDirection: "column",
-              boxShadow: "6",
-            }}
-          >
-            <SkillsetList id="other" />
-          </Paper>
-        </Grid>
-      </Grid>
-    </Container>
+    <Grid container justifyContent="center">
+      <Stack sx={{ width: "1100px" }}>
+        <SkillsetList id="front" />
+        <SkillsetList id="back" />
+        <SkillsetList id="other" />
+      </Stack>
+    </Grid>
   );
 };
 
